851644a1639aa9ce3b8f5d8afdfd8005ddba0357
Jasskaran <jsodhi22.kj@gmail.com>
2017-02-27 12:30:30 +0530

    Add files via upload

---
+++ longestCommonSubString.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int LCS(string str1,string str2)
+{
+	int l1=str1.size();
+	int l2=str2.size();
+	int table[l2+1][l1+1];
+	cout<<l1<<endl;
+	cout<<l2<<endl;
+	for(int i=0;i<=l2;i++)
+	{
+		for(int j=0;j<=l1;j++)
+		{
+			if(i==0|| j==0)
+				table[i][j]=0;
+			else if(str1[i-1]==str2[j-1])
+				table[i][j]=table[i-1][j-1]+1;
+			else
+				table[i][j]=table[i-1][j];
+		}
+	}
+	for(int i=0;i<=l2;i++)
+	{
+		for(int j=0;j<=l1;j++)
+			cout<<table[i][j]<<" ";
+		cout<<endl;
+	}
+	return 0;
+   
+}
+
+int main()
+{
+	string str1,str2;
+	cin>>str1>>str2;
+	int c=LCS(str1,str2);
+	cout<<c;
+}
