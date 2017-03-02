186b7719a1f7b7a89ff01db2fe87bcdda3ed96c2
Jasskaran <jsodhi22.kj@gmail.com>
2017-03-02 17:32:56 +0530

    Add files via upload

---
+++ SumWithoutAdj.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int SumWithoutAdj(int *arr,int N)
+{
+	int sum0=arr[0],sum1=arr[1];
+	for(int i=2;i<N;i++)
+	{
+		if((i%2)==0)
+			sum0=sum0+arr[i];
+		else
+			sum1=sum1+arr[i];
+	}
+	return (sum0>sum1)?sum0:sum1;
+}
+
+int main()
+{
+	int N;
+	cin>>N;
+	int arr[N];
+	for(int i=0;i<N;i++)
+		cin>>arr[i];
+	int c=SumWithoutAdj(arr,N);
+	cout<<c;
+}
